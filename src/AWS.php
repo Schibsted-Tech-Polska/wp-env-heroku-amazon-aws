c12cadad62478112ed07ae455de0315b72098803
Wojciech Krawczyk <wojtekk@gmail.com>
2016-06-07 10:58:48 +0200

    Add DBI_ prefix to AWS credentials
    Required by new plugin version

--- src/AWS.php
+++ src/AWS.php
@@ -2,9 +2,9 @@
 
 namespace Frc\WP\Env\Heroku\AWS;
 
-$aws_access_key_id = getenv('AWS_ACCESS_KEY_ID');
-$aws_secret_access_key = getenv('AWS_SECRET_ACCESS_KEY');
+$aws_access_key_id = getenv('DBI_AWS_ACCESS_KEY_ID');
+$aws_secret_access_key = getenv('DBI_AWS_SECRET_ACCESS_KEY');
 if ( $aws_access_key_id and $aws_secret_access_key ) {
-    define('AWS_ACCESS_KEY_ID', $aws_access_key_id);
-    define('AWS_SECRET_ACCESS_KEY', $aws_secret_access_key);
+    define('DBI_AWS_ACCESS_KEY_ID', $aws_access_key_id);
+    define('DBI_AWS_SECRET_ACCESS_KEY', $aws_secret_access_key);
 }
